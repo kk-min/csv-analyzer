b48fe2d74e4339c4d5765bd3294a393a9fa96171
mink0003 <76023265+kk-min@users.noreply.github.com>
2022-10-29 14:20:17 +0800

    Rename and fix processor functions

--- interface.py
+++ interface.py
@@ -1,10 +1,14 @@
 import streamlit as st
-import interface
+from csv_processor import *
+from io import StringIO
 
 def get_file():
     uploaded_file = st.file_uploader('Choose a file')
     if uploaded_file is not None:
         stringio = StringIO(uploaded_file.getvalue().decode('utf-8'))
+        st.session_state.uploaded_file = stringio
+        st.session_state.csv_data = read_file(stringio)
+        st.session_state.column_list = get_column_list(st.session_state.csv_data)
         return stringio
     else:
         return None
@@ -27,8 +31,7 @@ def interface_main():
        st.session_state.csv_data = None
     
     st.title('CSV Analayzer')
-    st.session_state.uploaded_file = get_file();
+    get_file();
     st.selectbox(label='X Axis:', options=st.session_state.column_list, key='x_axis')
     st.selectbox(label='Y Axis:', options=st.session_state.column_list, key='y_axis')
-    st.text("X Axis: "+st.session_state.x_axis)
-    st.text("Y Axis: "+st.session_state.y_axis)
+
