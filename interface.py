da294691bad51163e9bf34ea023d6df50f16d4f6
mink0003 <76023265+kk-min@users.noreply.github.com>
2022-10-29 13:59:10 +0800

    Add key to selectbox dropdowns

--- interface.py
+++ interface.py
@@ -2,9 +2,9 @@ import streamlit as st
 import interface
 
 def get_file():
-    uploaded_file = st.file_uploader("Choose a file")
+    uploaded_file = st.file_uploader('Choose a file')
     if uploaded_file is not None:
-        stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+        stringio = StringIO(uploaded_file.getvalue().decode('utf-8'))
         return stringio
     else:
         return None
@@ -20,18 +20,15 @@ def interface_main():
         st.session_state.uploaded_file = None
 
     if 'column_list' not in st.session_state:
-       st.session_state.column_list = []
+       st.session_state.column_list = ['Age', 'Gender']
     
     
     if 'csv_data' not in st.session_state:
        st.session_state.csv_data = None
-
-    if 'x_axis' not in st.session_state:
-        st.session_state.x_axis = ""
-
-        
-    if 'y_axis' not in st.session_state:
-        st.session_state.y_axis = ""
     
-    st.title("CSV Analayzer")
+    st.title('CSV Analayzer')
     st.session_state.uploaded_file = get_file();
+    st.selectbox(label='X Axis:', options=st.session_state.column_list, key='x_axis')
+    st.selectbox(label='Y Axis:', options=st.session_state.column_list, key='y_axis')
+    st.text("X Axis: "+st.session_state.x_axis)
+    st.text("Y Axis: "+st.session_state.y_axis)
