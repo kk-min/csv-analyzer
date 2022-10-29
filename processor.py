61324a848d590cbe63a1b7c94016550eeba5acdc
mink0003 <76023265+kk-min@users.noreply.github.com>
2022-10-29 14:02:02 +0800

    Add column reader to processor.py

--- processor.py
+++ processor.py
@@ -4,3 +4,9 @@ import seaborn as sb
 def read_file(file):
     csv_data = csv.reader(file, delimiter=",")
     return csv_data
+
+def get_columns(data):
+    column_list = []
+    for row in data:
+        column_list.append(row)
+        return column_list
